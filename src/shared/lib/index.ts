440410f34a0bbd99bbb3b80d5a9c583f5fb273d1
franchukv <franchuk.vldr@gmail.com>
2026-04-23 15:27:21 +0200

    rename userSlice to index

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,4 +1,3 @@
 export { generatePageTitle } from './utils';
 export { isApiError } from './guards';
 export { useAppDispatch, useAppSelector } from './hooks';
-export { authSchema, authUpdatePasswordSchema } from './validation';
