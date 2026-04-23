3492a0affedd3e026559a981a1335845a3a1380e
franchukv <franchuk.vldr@gmail.com>
2026-04-23 20:53:43 +0200

    added basic style and markup to NotePage

---
+++ src/pages/note/ui/NotePage.tsx
@@ -0,0 +1,48 @@
+import { usePageTitle } from '@/shared/lib';
+
+export const NotePage = () => {
+  const note = {
+    title: 'Note1',
+  };
+
+  usePageTitle(note?.title);
+
+  return (
+    <div className="p-4">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, magni
+      quae modi reprehenderit odit, doloremque nemo ratione, minima porro vero
+      aspernatur molestiae temporibus a repudiandae blanditiis voluptatem
+      dolorum tempora dolorem. Architecto, quidem corporis, consequatur quod
+      possimus, esse impedit voluptate sed accusantium ab ipsum molestiae
+      ducimus neque mollitia minus placeat repellat temporibus facere
+      dignissimos cupiditate. Maiores excepturi quibusdam quod accusamus
+      reprehenderit! Ratione totam iusto explicabo? Accusantium maxime tempore
+      cum modi corporis, consequatur nesciunt odit velit laudantium asperiores
+      aliquam placeat eaque eum dolores numquam sed impedit unde recusandae
+      vitae necessitatibus pariatur perferendis? Ducimus qui ut sunt error
+      asperiores enim, fuga atque earum officiis explicabo perspiciatis
+      voluptate molestias, ab suscipit necessitatibus accusantium quod itaque
+      sed aperiam neque! Illum blanditiis consequuntur doloremque eum aliquam!
+      Ea aspernatur blanditiis, minus voluptatem alias quasi voluptatum quia
+      unde accusantium, provident tempore architecto placeat officia commodi.
+      Illum velit asperiores itaque praesentium rem fuga, saepe optio at vero,
+      commodi recusandae. Animi quis magni sit alias, quibusdam sapiente
+      voluptas eveniet non, excepturi ipsam corporis cumque culpa eos sint
+      reiciendis eligendi natus consequuntur, odio ipsum corrupti! Ipsa nulla
+      cupiditate alias a eveniet? Quis repellat ipsa tempore magnam ad vero
+      culpa sed distinctio eveniet, ratione velit iure maiores tempora possimus
+      asperiores sit. Fugit voluptate maiores similique perferendis cumque error
+      aspernatur ut! Ipsum, fuga. Temporibus impedit ducimus quia repellat
+      aperiam, eius fuga culpa, neque dolore ut tempora! Officiis molestias
+      tempora, deserunt nihil explicabo vero alias illum beatae eum quisquam
+      similique fuga corrupti soluta! Qui. Maxime minus aspernatur qui, eligendi
+      accusamus dolor facere. Explicabo id, reiciendis repudiandae architecto,
+      sequi distinctio voluptates quos cumque mollitia, consequatur soluta
+      sapiente quidem impedit tempore quibusdam doloremque eveniet dolor
+      voluptate. Ab officia reiciendis doloribus autem accusantium voluptatem
+      animi temporibus explicabo molestias. Commodi ipsum, at, eaque totam,
+      error cupiditate iste necessitatibus veritatis omnis repellendus ducimus
+      quo temporibus officia est architecto asperiores.
+    </div>
+  );
+};
