3ee59fdceadc2e21ae79ac6cfa422f3ea3cc4c43
franchukv <franchuk.vldr@gmail.com>
2026-04-23 19:06:20 +0200

    added TagList component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,6 @@
 export { FormLayout } from './form-layout/FormLayout';
 export { Button } from './button/Button';
+export { NavigationButton } from './navigation-button/NavigationButton';
 export { InputField } from './input-field/InputField';
 export { Hint } from './hint/Hint';
+export { TagsList } from './tags-list/TagsList';
--- src/shared/ui/tags-list/TagsList.tsx
+++ src/shared/ui/tags-list/TagsList.tsx
@@ -5,12 +5,14 @@ interface TagsListProps {
 
 export const TagsList = ({ title, children }: TagsListProps) => {
   return (
-    <div className="w-full flex flex-col gap-4 lg:gap-2">
-      <h2 className="text-preset-1 lg:text-preset-4 lg:text-neutral-500">
+    <div className="pt-2 w-full flex flex-col gap-4 lg:gap-2">
+      <h2 className="max-lg:text-preset-1 lg:text-preset-4 lg:text-neutral-500">
         {title}
       </h2>
 
-      <div className="w-full flex flex-col lg:gap-1">{children}</div>
+      <div className="w-full flex flex-col lg:gap-1 overflow-auto lg:max-h-[calc(100dvh-219px)] lg:min-h-60">
+        {children}
+      </div>
     </div>
   );
 };
