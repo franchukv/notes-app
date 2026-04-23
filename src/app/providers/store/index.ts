d7ae66bfa01727a11fa4f2b25addf18cbe43c8e1
franchukv <franchuk.vldr@gmail.com>
2026-04-23 15:37:11 +0200

    added responsive slice into store

--- src/app/providers/store/index.ts
+++ src/app/providers/store/index.ts
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import { supabaseAuthMiddleware } from './middleware/supabaseMiddleware';
 import { userSlice } from '@/entities/user';
 import { supabaseApi } from '@/shared/api';
+import { responsiveSlice } from '@/shared/model';
 
 export const store = configureStore({
   reducer: {
+    responsive: responsiveSlice.reducer,
     user: userSlice.reducer,
     [supabaseApi.reducerPath]: supabaseApi.reducer,
   },
