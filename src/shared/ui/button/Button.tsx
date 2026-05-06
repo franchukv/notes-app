eecbff999084009e99581100841281a685deffbb
franchukv <franchuk.vldr@gmail.com>
2026-05-06 21:13:54 +0200

    impoved Button component to be more utility

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -3,7 +3,12 @@ import type { ComponentPropsWithoutRef, ElementType } from 'react';
 
 type ButtonProps<T extends ElementType> = {
   as?: T;
-  variant?: 'primary' | 'secondary' | 'border';
+  variant?:
+    | 'primary'
+    | 'secondary'
+    | 'border'
+    | 'primary-link'
+    | 'secondary-link';
 } & Omit<ComponentPropsWithoutRef<T>, 'variant'>;
 
 export const Button = <T extends ElementType = 'button'>({
@@ -14,13 +19,20 @@ export const Button = <T extends ElementType = 'button'>({
   ...restProps
 }: ButtonProps<T>) => {
   const classNames = cn(
-    'py-3 px-4 flex items-center justify-center gap-2 text-center text-preset-4 rounded-lg transition-all duration-300 cursor-pointer border focus-visible:bg-blue-500 disabled:bg-neutral-100 disabled:text-neutral-300! disabled:border-neutral-100! disabled:pointer-events-none',
+    'flex items-center justify-center gap-2 text-center transition-all duration-300 cursor-pointer disabled:pointer-events-none [&>svg]:h-5 [&>svg]:w-5',
+    !variant.includes('link')
+      ? 'py-3 px-4 text-preset-4 rounded-lg border focus-visible:bg-blue-500 disabled:bg-neutral-100 disabled:text-neutral-300! disabled:border-neutral-100!'
+      : 'text-preset-5',
     variant === 'primary' &&
       'text-white bg-blue-500 border-blue-500 not-focus-visible:hover:bg-blue-700 not-focus-visible:hover:border-blue-700 not-focus-visible:active:border-blue-700 not-focus-visible:active:bg-blue-700',
     variant === 'secondary' &&
       'text-neutral-600 bg-neutral-100 border-neutral-100 hover:bg-white active:bg-white hover:text-neutral-950 active:text-neutral-950 hover:border-neutral-300 active:border-neutral-300 focus-visible:bg-white focus-visible:text-neutral-950 focus-visible:border-neutral-950',
     variant === 'border' &&
       'text-neutral-950 bg-white border-neutral-300 hover:text-neutral-600 active:text-neutral-600 hover:bg-neutral-100 active:bg-neutral-100 hover:border-neutral-100 active:border-neutral-100 focus-visible:bg-white focus-visible:text-neutral-950 focus-visible:border-neutral-950',
+    variant === 'primary-link' &&
+      'text-blue-500 hover:text-blue-700 active:text-blue-700',
+    variant === 'secondary-link' &&
+      'text-neutral-600 hover:text-neutral-950 active:text-neutral-950',
     className,
   );
 
