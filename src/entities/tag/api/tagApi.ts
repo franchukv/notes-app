aa9973f1f7e81e56d346a3d7549d7a81f800bcd1
franchukv <franchuk.vldr@gmail.com>
2026-05-06 21:05:42 +0200

    added getTagBySlug to tagApi

--- src/entities/tag/api/tagApi.ts
+++ src/entities/tag/api/tagApi.ts
@@ -2,8 +2,33 @@ import type { Tag } from '../model/types';
 import { mapTag } from './mappers';
 import { supabase, supabaseApi } from '@/shared/api';
 
+interface GetTagBySlugArgs {
+  slug: string;
+}
+
 export const tagApi = supabaseApi.injectEndpoints({
   endpoints: (build) => ({
+    getTagBySlug: build.query<Tag, GetTagBySlugArgs>({
+      queryFn: async ({ slug }) => {
+        const { data, error } = await supabase
+          .from('tags')
+          .select('*')
+          .eq('slug', slug)
+          .single();
+
+        if (error) {
+          return {
+            error: {
+              status: 400,
+              data: { message: error.message },
+            },
+          };
+        }
+
+        return { data: mapTag(data) };
+      },
+      providesTags: ['Tag'],
+    }),
     getTags: build.query<Tag[], void>({
       queryFn: async () => {
         const { data, error } = await supabase.from('tags').select('*');
@@ -24,4 +49,4 @@ export const tagApi = supabaseApi.injectEndpoints({
   }),
 });
 
-export const { useGetTagsQuery } = tagApi;
+export const { useGetTagBySlugQuery, useGetTagsQuery } = tagApi;
