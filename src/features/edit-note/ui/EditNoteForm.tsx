a0f56fb0baf57833115ad90d1b5287ed1c854ff5
franchukv <franchuk.vldr@gmail.com>
2026-05-06 21:17:58 +0200

    added edit note functionality

---
+++ src/features/edit-note/ui/EditNoteForm.tsx
@@ -0,0 +1,135 @@
+import { z } from 'zod';
+import cn from 'classnames';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { noteSchema, useUpdateNoteMutation, type Note } from '@/entities/note';
+import { useGetTagsQuery } from '@/entities/tag';
+import { formatDate, useAppSelector } from '@/shared/lib';
+import { Button, CreatableMultiSelect, Hint } from '@/shared/ui';
+import { selectIsDesktop } from '@/shared/model';
+import TagIcon from '@/shared/assets/icons/tag-icon.svg?react';
+import ClockIcon from '@/shared/assets/icons/clock-icon.svg?react';
+
+interface EditNoteFormProps {
+  note: Note;
+  parentUrl: string;
+}
+
+type EditNoteFormData = z.infer<typeof noteSchema>;
+
+export const EditNoteForm = ({ note, parentUrl }: EditNoteFormProps) => {
+  const navigate = useNavigate();
+
+  const isDesktop = useAppSelector(selectIsDesktop);
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<EditNoteFormData>({
+    resolver: zodResolver(noteSchema),
+    defaultValues: {
+      title: note.title,
+      tags: note.tags
+        ? note.tags.map((tag) => ({ value: tag.slug, label: tag.name }))
+        : [],
+      content: note.content,
+    },
+  });
+
+  const { data: tags, isLoading: isTagsLoading } = useGetTagsQuery();
+  const [updateNote] = useUpdateNoteMutation();
+
+  const options = tags
+    ? tags.map((tag) => ({ value: tag.slug, label: tag.name }))
+    : [];
+
+  const onSubmit = async ({ title, tags, content }: EditNoteFormData) => {
+    const response = await updateNote({
+      id: note.id,
+      tags: tags ? tags.map((tag) => tag.label) : [],
+      title,
+      content: content ?? '',
+    });
+
+    if ('error' in response) {
+      return;
+    }
+
+    navigate(`/notes/${note.slug}`);
+  };
+
+  return (
+    <form
+      id="create-note-form"
+      className="h-full flex flex-col gap-3 sm:gap-4"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <fieldset className="pb-3 flex flex-col gap-3 border-b border-neutral-200 sm:pb-4 sm:gap-4">
+        <div className="flex flex-col gap-1.5">
+          <input
+            {...register('title')}
+            type="text"
+            placeholder="Enter a title…"
+            className={cn(
+              'p-0 text-preset-2 border-b border-transparent focus:border-neutral-950 outline-none placeholder:text-neutral-700 sm:text-preset-1',
+              errors.title?.message && 'border-red-500!',
+            )}
+          />
+          {errors.title?.message && (
+            <Hint type="error" text={errors.title.message} />
+          )}
+        </div>
+
+        <div className="flex flex-col gap-2 text-preset-6 text-neutral-700 sm:text-preset-5">
+          <div className="flex items-center gap-2">
+            <div className="min-w-28.75 flex items-center gap-1.5">
+              <TagIcon className="h-4 w-4" /> Tags
+            </div>
+
+            <CreatableMultiSelect
+              name="tags"
+              control={control}
+              isLoading={isTagsLoading}
+              options={options}
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <div className="min-w-28.75 flex items-center gap-1.5">
+              <ClockIcon className="h-4 w-4" /> Last edited
+            </div>
+
+            <span className="text-neutral-700">
+              {formatDate(note.updatedAt)}
+            </span>
+          </div>
+        </div>
+      </fieldset>
+
+      <fieldset className="w-full h-full flex flex-col gap-3 sm:gap-4">
+        <textarea
+          {...register('content')}
+          placeholder="Start typing your note here…"
+          className="w-full min-h-60 h-full outline-none resize-none text-preset-6 placeholder:text-neutral-700 sm:text-preset-5"
+        ></textarea>
+      </fieldset>
+
+      {isDesktop && (
+        <div className="pt-4 flex items-center gap-4 border-t border-neutral-200">
+          <Button type="submit">Save Note</Button>
+
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={() => navigate(parentUrl)}
+          >
+            Cancel
+          </Button>
+        </div>
+      )}
+    </form>
+  );
+};
