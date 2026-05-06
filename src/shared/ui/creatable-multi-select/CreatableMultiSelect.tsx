a9522e090002aef294e3a66d6ae2d538ab6088fc
franchukv <franchuk.vldr@gmail.com>
2026-05-06 21:13:10 +0200

    added CreatableMultiSelect component

--- src/shared/ui/creatable-multi-select/CreatableMultiSelect.tsx
+++ src/shared/ui/creatable-multi-select/CreatableMultiSelect.tsx
@@ -4,20 +4,87 @@ import {
   type FieldValues,
   type Path,
 } from 'react-hook-form';
+import cn from 'classnames';
+import { components } from 'react-select';
 import CreatableSelect from 'react-select/creatable';
-import type { GroupBase, OptionsOrGroups } from 'react-select';
+import type {
+  ClassNamesConfig,
+  ClearIndicatorProps,
+  DropdownIndicatorProps,
+  GroupBase,
+  MultiValueRemoveProps,
+  OptionsOrGroups,
+} from 'react-select';
+import CloseIcon from '../../assets/icons/close-icon.svg?react';
+import BackArrow from '../../assets/icons/back-arrow-icon.svg?react';
 
 interface Option {
   label: string;
   value: string;
 }
-
 interface CreatableMultiSelectProps<T extends FieldValues> {
   name: Path<T>;
   control: Control<T>;
   options: OptionsOrGroups<Option, GroupBase<Option>>;
   isLoading: boolean;
 }
+
+const classNames: ClassNamesConfig<Option, true, GroupBase<Option>> = {
+  container: () => 'w-full text-neutral-950 text-preset-6! sm:text-preset-5!',
+  control: ({ isFocused }) =>
+    cn(
+      'min-h-0! border! rounded-lg! shadow-[0_1px_2px_0_rgba(10,13,20,0.03)]! outline-2! outline-offset-2!',
+      isFocused
+        ? 'border-neutral-950! outline-neutral-400!'
+        : 'border-neutral-300! outline-transparent! hover:bg-neutral-50!',
+    ),
+  valueContainer: () => 'p-1!',
+  multiValue: () =>
+    'py-0.5! pl-1.5! pr-1! flex items-center gap-1 bg-neutral-200! rounded-sm!',
+  multiValueLabel: () => 'p-0! text-preset-6!',
+  multiValueRemove: () =>
+    'p-0! cursor-pointer bg-transparent! hover:text-red-500',
+  indicatorsContainer: () => '',
+  indicatorSeparator: () => 'bg-neutral-300!',
+  loadingIndicator: () => 'text-neutral-500!',
+  clearIndicator: () =>
+    'p-2! text-neutral-500! cursor-pointer hover:text-red-500!',
+  dropdownIndicator: ({ selectProps }) =>
+    cn(
+      'p-2! text-neutral-500! cursor-pointer transition-all! duration-150! hover:text-neutral-950!',
+      selectProps.menuIsOpen && 'text-neutral-950! scale-y-[-1]',
+    ),
+  menu: () =>
+    'rounded-lg! shadow-[0_1px_2px_0_rgba(10,13,20,0.03)]! border border-neutral-300 overflow-hidden!',
+  menuList: () => 'p-0!',
+  option: ({ isFocused }) =>
+    cn('p-2! cursor-pointer!', isFocused && 'bg-neutral-100!'),
+};
+
+const MultiValueRemove = (
+  props: MultiValueRemoveProps<Option, true, GroupBase<Option>>,
+) => (
+  <components.MultiValueRemove {...props}>
+    <CloseIcon className="h-3 w-3" />
+  </components.MultiValueRemove>
+);
+
+const ClearIndicator = (
+  props: ClearIndicatorProps<Option, true, GroupBase<Option>>,
+) => (
+  <components.ClearIndicator {...props}>
+    <CloseIcon className="h-4 w-4" />
+  </components.ClearIndicator>
+);
+
+const DropdownIndicator = (
+  props: DropdownIndicatorProps<Option, true, GroupBase<Option>>,
+) => (
+  <components.DropdownIndicator {...props}>
+    <BackArrow className="h-4 w-4 -rotate-90" />
+  </components.DropdownIndicator>
+);
+
 export const CreatableMultiSelect = <T extends FieldValues>({
   name,
   control,
@@ -35,7 +102,12 @@ export const CreatableMultiSelect = <T extends FieldValues>({
           isClearable
           isLoading={isLoading}
           options={options}
-          className="w-full"
+          classNames={classNames}
+          components={{
+            MultiValueRemove,
+            ClearIndicator,
+            DropdownIndicator,
+          }}
         />
       )}
     />
