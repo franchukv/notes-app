0949797d0570ef7592a0b2bf404ca34aa1943967
franchukv <franchuk.vldr@gmail.com>
2026-05-06 21:17:04 +0200

    finish NotePage component

--- src/pages/note/ui/NotePage.tsx
+++ src/pages/note/ui/NotePage.tsx
@@ -1,48 +1,83 @@
-import { usePageTitle } from '@/shared/lib';
+import { useLocation, useMatches, useParams, type UIMatch } from 'react-router';
+import { ActionBarWidget } from '@/widgets/action-bar';
+import { NoteActionsWidget } from '@/widgets/note-actions';
+import { NoteContent } from '@/widgets/note-content';
+import { useGetNoteBySlugQuery } from '@/entities/note';
+import { useAppSelector, usePageTitle } from '@/shared/lib';
+import { selectIsDesktop } from '@/shared/model';
 
 export const NotePage = () => {
-  const note = {
-    title: 'Note1',
-  };
+  const { pathname } = useLocation();
+  const { tagSlug, noteSlug } = useParams();
+  const matches = useMatches() as UIMatch<unknown, { title?: string }>[];
 
-  usePageTitle(note?.title);
+  const isDesktop = useAppSelector(selectIsDesktop);
+  const headerTitle = matches[matches.length - 2].handle?.title;
+
+  const isArchivedNote = pathname.includes('/archived-notes');
+  const parentUrl = isArchivedNote
+    ? '/archived-notes'
+    : tagSlug
+      ? `/tags/${tagSlug}`
+      : '/notes';
+
+  const {
+    data: note,
+    isLoading,
+    isSuccess,
+  } = useGetNoteBySlugQuery({ slug: noteSlug! });
+
+  usePageTitle({
+    title: note?.title,
+    headerTitle: headerTitle,
+    previousHeaderTitle: true,
+  });
 
   return (
-    <div className="p-4">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, magni
-      quae modi reprehenderit odit, doloremque nemo ratione, minima porro vero
-      aspernatur molestiae temporibus a repudiandae blanditiis voluptatem
-      dolorum tempora dolorem. Architecto, quidem corporis, consequatur quod
-      possimus, esse impedit voluptate sed accusantium ab ipsum molestiae
-      ducimus neque mollitia minus placeat repellat temporibus facere
-      dignissimos cupiditate. Maiores excepturi quibusdam quod accusamus
-      reprehenderit! Ratione totam iusto explicabo? Accusantium maxime tempore
-      cum modi corporis, consequatur nesciunt odit velit laudantium asperiores
-      aliquam placeat eaque eum dolores numquam sed impedit unde recusandae
-      vitae necessitatibus pariatur perferendis? Ducimus qui ut sunt error
-      asperiores enim, fuga atque earum officiis explicabo perspiciatis
-      voluptate molestias, ab suscipit necessitatibus accusantium quod itaque
-      sed aperiam neque! Illum blanditiis consequuntur doloremque eum aliquam!
-      Ea aspernatur blanditiis, minus voluptatem alias quasi voluptatum quia
-      unde accusantium, provident tempore architecto placeat officia commodi.
-      Illum velit asperiores itaque praesentium rem fuga, saepe optio at vero,
-      commodi recusandae. Animi quis magni sit alias, quibusdam sapiente
-      voluptas eveniet non, excepturi ipsam corporis cumque culpa eos sint
-      reiciendis eligendi natus consequuntur, odio ipsum corrupti! Ipsa nulla
-      cupiditate alias a eveniet? Quis repellat ipsa tempore magnam ad vero
-      culpa sed distinctio eveniet, ratione velit iure maiores tempora possimus
-      asperiores sit. Fugit voluptate maiores similique perferendis cumque error
-      aspernatur ut! Ipsum, fuga. Temporibus impedit ducimus quia repellat
-      aperiam, eius fuga culpa, neque dolore ut tempora! Officiis molestias
-      tempora, deserunt nihil explicabo vero alias illum beatae eum quisquam
-      similique fuga corrupti soluta! Qui. Maxime minus aspernatur qui, eligendi
-      accusamus dolor facere. Explicabo id, reiciendis repudiandae architecto,
-      sequi distinctio voluptates quos cumque mollitia, consequatur soluta
-      sapiente quidem impedit tempore quibusdam doloremque eveniet dolor
-      voluptate. Ab officia reiciendis doloribus autem accusantium voluptatem
-      animi temporibus explicabo molestias. Commodi ipsum, at, eaque totam,
-      error cupiditate iste necessitatibus veritatis omnis repellendus ducimus
-      quo temporibus officia est architecto asperiores.
+    <div className="w-full flex">
+      <div className="h-full w-full py-5 flex flex-col lg:border-r lg:border-neutral-200 lg:max-w-[calc(100%-(180px))] xl:max-w-[calc(100%-290px)]">
+        <div className="custom-container h-full flex">
+          <div className="w-full flex flex-col gap-3 sm:gap-4">
+            {!isDesktop && (
+              <ActionBarWidget parentUrl={parentUrl}>
+                {isSuccess && (
+                  <NoteActionsWidget
+                    parentUrl={parentUrl}
+                    note={note}
+                    isArchivedNote={isArchivedNote}
+                  />
+                )}
+              </ActionBarWidget>
+            )}
+
+            {isSuccess ? (
+              <NoteContent note={note} />
+            ) : isLoading ? (
+              <div className="m-auto text-preset-3 text-center">
+                Note is loading...
+              </div>
+            ) : (
+              <div className="m-auto text-preset-3 text-center">
+                Note not found
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {isDesktop && isSuccess && (
+        <aside className="h-full w-full py-5 flex flex-col gap-3 lg:max-w-45 xl:max-w-72.5">
+          <div className="custom-container">
+            <div className="flex flex-col gap-3">
+              <NoteActionsWidget
+                parentUrl={parentUrl}
+                note={note}
+                isArchivedNote={isArchivedNote}
+              />
+            </div>
+          </div>
+        </aside>
+      )}
     </div>
   );
 };
