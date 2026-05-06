fcf3117ba832daed9dc5094100d8d0e8ecc2faa0
franchukv <franchuk.vldr@gmail.com>
2026-05-06 21:12:52 +0200

    decrease gap into Hint component

--- src/shared/ui/hint/Hint.tsx
+++ src/shared/ui/hint/Hint.tsx
@@ -8,7 +8,7 @@ interface HintProps {
 
 export const Hint = ({ text, type = 'default' }: HintProps) => {
   const classNames = cn(
-    'flex items-center gap-2 text-neutral-600 text-12',
+    'flex items-center gap-1.5 text-neutral-600 text-12',
     type === 'error' && 'text-red-500',
   );
 
