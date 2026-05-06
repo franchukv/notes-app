795ae65fc4a5781487d29097515254e37e098da5
franchukv <franchuk.vldr@gmail.com>
2026-05-06 21:00:56 +0200

    finish NotesList

--- src/widgets/notes-list/ui/NotesList.tsx
+++ src/widgets/notes-list/ui/NotesList.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router';
-import { NoteItem, type Note } from '@/entities/note';
+import { NoteItem, SkeletonNoteItem, type Note } from '@/entities/note';
 import { Button } from '@/shared/ui';
 import { selectIsDesktop } from '@/shared/model';
 import { useAppSelector } from '@/shared/lib';
@@ -8,42 +8,54 @@ import PlusIcon from '@/shared/assets/icons/plus-icon.svg?react';
 interface NotesListProps {
   parentUrl: string;
   notes: Note[];
+  isLoading: boolean;
+  hasCreateNewNoteButton?: boolean;
   children?: React.ReactNode;
 }
 
-export const NotesList = ({ parentUrl, notes, children }: NotesListProps) => {
+export const NotesList = ({
+  parentUrl,
+  notes,
+  isLoading,
+  hasCreateNewNoteButton = true,
+  children,
+}: NotesListProps) => {
   const isDesktop = useAppSelector(selectIsDesktop);
 
   return (
-    <div className="w-full py-5 lg:max-w-72.5 lg:min-w-72.5 lg:border-r lg:border-neutral-200">
+    <div className="w-full py-5 lg:max-w-60 lg:border-r lg:border-neutral-200 xl:max-w-72.5 ">
       <div className="custom-container">
         <div className="flex flex-col gap-4">
-          <Button
-            as={Link}
-            to="/create-new-note"
-            className="fixed bottom-18 right-4 z-1 sm:bottom-26.5 sm:right-8 max-sm:h-12 max-sm:w-12 max-lg:h-16 max-lg:w-16 max-lg:p-0 max-lg:rounded-full max-lg:shadow-[0_7px_11px_0_rgba(202,207,216,0.7)] lg:static"
-          >
-            {isDesktop ? '+ Create New Note' : <PlusIcon />}
-          </Button>
+          {hasCreateNewNoteButton && (
+            <Button
+              as={Link}
+              to={`${parentUrl}/create-new-note`}
+              className="fixed bottom-18 right-4 z-1 sm:bottom-26.5 sm:right-8 max-sm:h-12 max-sm:w-12 max-lg:h-16 max-lg:w-16 max-lg:p-0 max-lg:rounded-full max-lg:shadow-[0_7px_11px_0_rgba(202,207,216,0.7)] lg:static"
+            >
+              {isDesktop ? '+ Create New Note' : <PlusIcon />}
+            </Button>
+          )}
+          {isLoading ? (
+            <div className="flex flex-col">
+              <SkeletonNoteItem />
+              <SkeletonNoteItem />
+              <SkeletonNoteItem />
+              <SkeletonNoteItem />
+              <SkeletonNoteItem />
+            </div>
+          ) : (
+            <>
+              {children}
 
-          {children}
-
-          <div className="flex flex-col">
-            {notes.length > 0 ? (
-              notes.map((note) => (
-                <NoteItem
-                  key={note.id}
-                  url={`${parentUrl}/${note.slug}`}
-                  {...note}
-                />
-              ))
-            ) : (
-              <p>
-                You don’t have any notes yet. Start a new note to capture your
-                thoughts and ideas.
-              </p>
-            )}
-          </div>
+              {notes.length > 0 && (
+                <div className="flex flex-col">
+                  {notes.map((note) => (
+                    <NoteItem key={note.id} parentUrl={parentUrl} {...note} />
+                  ))}
+                </div>
+              )}
+            </>
+          )}
         </div>
       </div>
     </div>
