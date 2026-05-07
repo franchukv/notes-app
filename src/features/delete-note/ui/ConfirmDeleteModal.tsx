8ec1e54b59eb46b13ac8da510e43f6e23bd654ba
franchukv <franchuk.vldr@gmail.com>
2026-05-07 12:57:49 +0200

    added delete-note feature

---
+++ src/features/delete-note/ui/ConfirmDeleteModal.tsx
@@ -0,0 +1,66 @@
+import { useNavigate } from 'react-router';
+import { useDeleteNoteMutation } from '@/entities/note';
+import { Button, Modal } from '@/shared/ui';
+import DeleteIcon from '@/shared/assets/icons/delete-icon.svg?react';
+
+interface ConfirmDeleteModalProps {
+  isOpen: boolean;
+  noteId: number;
+  parentUrl: string;
+  onClose: () => void;
+}
+
+export const ConfirmDeleteModal = ({
+  isOpen,
+  noteId,
+  parentUrl,
+  onClose,
+}: ConfirmDeleteModalProps) => {
+  const navigate = useNavigate();
+
+  const [deleteNote, { isLoading }] = useDeleteNoteMutation();
+
+  const handleConfirm = async () => {
+    const { error } = await deleteNote({ id: noteId });
+
+    if (error) {
+      return;
+    }
+
+    onClose();
+    navigate(parentUrl);
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="modal">
+        <div className="flex flex-col">
+          <div className="p-5 flex gap-4">
+            <div className="h-10 w-10 flex items-center justify-center bg-neutral-100 rounded-lg">
+              <DeleteIcon className="h-6 w-6" />
+            </div>
+
+            <div className="flex flex-col gap-1.5 text-neutral-700">
+              <h3 className="text-preset-3 text-neutral-950">Delete Note</h3>
+
+              <p className="text-preset-5">
+                Are you sure you want to permanently delete this note? <br />
+                This action cannot be undone.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full py-4 px-5 flex items-center justify-end gap-4 border-t border-neutral-200">
+            <Button variant="secondary" onClick={onClose} disabled={isLoading}>
+              Cancel
+            </Button>
+
+            <Button variant="red" onClick={handleConfirm} disabled={isLoading}>
+              Delete Note
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
