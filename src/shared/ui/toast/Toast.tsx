dc95da2fa3382f192cd8b96ce64c0837091517af
franchukv <franchuk.vldr@gmail.com>
2026-05-13 12:29:23 +0200

    memoized handleClose for toasts

--- src/shared/ui/toast/Toast.tsx
+++ src/shared/ui/toast/Toast.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { Link } from 'react-router';
 import type { Toast as ToastType } from '../../model';
 import SuccessIcon from '../../assets/icons/success-icon.svg?react';
@@ -5,41 +6,43 @@ import ErrorIcon from '../../assets/icons/error-icon.svg?react';
 import InfoIcon from '../../assets/icons/info-icon.svg?react';
 import CloseIcon from '../../assets/icons/close-icon.svg?react';
 
-interface ToastProps extends Pick<ToastType, 'message' | 'type' | 'link'> {
-  onClose: () => void;
+const icons = {
+  success: SuccessIcon,
+  error: ErrorIcon,
+  info: InfoIcon,
+};
+
+interface ToastProps extends Pick<
+  ToastType,
+  'id' | 'message' | 'type' | 'link'
+> {
+  onClose: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const Toast = ({
-  message,
-  type = 'success',
-  link,
-  onClose,
-}: ToastProps) => {
-  const icons = {
-    success: SuccessIcon,
-    error: ErrorIcon,
-    info: InfoIcon,
-  };
-  const Icon = icons[type];
+export const Toast = memo(
+  ({ id, message, type = 'success', link, onClose }: ToastProps) => {
+    const Icon = icons[type];
 
-  return (
-    <div className="p-2 flex items-center gap-2 text-preset-6 rounded-lg border border-neutral-200 bg-white shadow-[0_16px_32px_-12px_rgba(14,18,27,0.1)] sm:text-preset-5 dark:bg-neutral-800 dark:border-neutral-700">
-      <Icon className="min-w-4 h-4 w-4 sm:min-w-5 sm:w-5 sm:h-5" />
+    return (
+      <div className="p-2 flex items-center gap-2 text-preset-6 rounded-lg border border-neutral-200 bg-white shadow-[0_16px_32px_-12px_rgba(14,18,27,0.1)] sm:text-preset-5 dark:bg-neutral-800 dark:border-neutral-700">
+        <Icon className="min-w-4 h-4 w-4 sm:min-w-5 sm:w-5 sm:h-5" />
 
-      <div className="mr-auto">{message}</div>
+        <div className="mr-auto">{message}</div>
 
-      {link && (
-        <Link to={link.url} className="ml-4 underline-link">
-          {link.text}
-        </Link>
-      )}
+        {link && (
+          <Link to={link.url} className="ml-4 underline-link">
+            {link.text}
+          </Link>
+        )}
 
-      <button
-        onClick={onClose}
-        className="min-w-4 h-4 w-4 text-neutral-400 cursor-pointer hover:text-red-500 sm:min-w-5 sm:w-5 sm:h-5"
-      >
-        <CloseIcon className="h-full w-full" />
-      </button>
-    </div>
-  );
-};
+        <button
+          onClick={onClose}
+          className="min-w-4 h-4 w-4 text-neutral-400 cursor-pointer hover:text-red-500 sm:min-w-5 sm:w-5 sm:h-5"
+          data-id={id}
+        >
+          <CloseIcon className="h-full w-full" />
+        </button>
+      </div>
+    );
+  },
+);
--- src/widgets/toasts-manager/ui/ToastsManager.tsx
+++ src/widgets/toasts-manager/ui/ToastsManager.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import { useAppSelector, useAppDispatch } from '@/shared/lib';
 import { Toast } from '@/shared/ui';
@@ -7,20 +8,19 @@ export const ToastsManager = () => {
   const toasts = useAppSelector(selectToasts);
   const dispatch = useAppDispatch();
 
-  const handleClose = (id: number) => {
-    dispatch(removeToast(id));
-  };
+  const handleClose = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      const id = Number(e.currentTarget.dataset.id);
+
+      dispatch(removeToast(id));
+    },
+    [dispatch],
+  );
 
   return createPortal(
     <div className="fixed bottom-18.5 right-4 z-9998  max-sm:max-w-[calc(100dvw-32px)] sm:max-w-100 min-w-72 flex flex-col gap-2 sm:bottom-24 sm:right-8 lg:bottom-16 lg:right-16">
       {toasts.map((toast) => (
-        <Toast
-          key={toast.id}
-          type={toast.type}
-          message={toast.message}
-          link={toast.link}
-          onClose={() => handleClose(toast.id)}
-        />
+        <Toast key={toast.id} onClose={handleClose} {...toast} />
       ))}
     </div>,
     document.body,
