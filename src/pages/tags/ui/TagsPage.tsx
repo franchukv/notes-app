cfb21b6ca661f3677ce86c189880a951a10620dc
franchukv <franchuk.vldr@gmail.com>
2026-05-13 21:41:07 +0200

    switched TagsList shared to widget

--- src/pages/tags/ui/TagsPage.tsx
+++ src/pages/tags/ui/TagsPage.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet, useParams } from 'react-router';
-import { TagButton, useGetTagsQuery } from '@/entities/tag';
+import { TagsList } from '@/widgets/tags-list';
+import { useGetTagsQuery } from '@/entities/tag';
 import { useAppSelector } from '@/shared/lib';
-import { Notice, TagsList } from '@/shared/ui';
 import { selectIsDesktop } from '@/shared/model';
 
 export const TagsPage = () => {
@@ -9,7 +9,7 @@ export const TagsPage = () => {
 
   const isDesktop = useAppSelector(selectIsDesktop);
 
-  const { data: tags } = useGetTagsQuery();
+  const { data: tags, isLoading } = useGetTagsQuery();
 
   if (isDesktop && !tagSlug) {
     return <Navigate to="/notes" replace />;
@@ -20,17 +20,7 @@ export const TagsPage = () => {
       {!isDesktop && !tagSlug ? (
         <div className="py-5">
           <div className="custom-container">
-            <TagsList title="Tags">
-              {tags && tags.length > 0 ? (
-                tags.map((tag) => (
-                  <TagButton key={tag.id} url={`/tags/${tag.slug}`}>
-                    {tag.name}
-                  </TagButton>
-                ))
-              ) : (
-                <Notice>There are no tags yet.</Notice>
-              )}
-            </TagsList>
+            <TagsList title="Tags" tags={tags ?? []} isLoading={isLoading} />
           </div>
         </div>
       ) : (
