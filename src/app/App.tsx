58cd9832de1ad32ce9e17be4a28153b110b3bb52
franchukv <franchuk.vldr@gmail.com>
2026-05-13 14:04:41 +0200

    added SpeedInsights

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,12 @@
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { AppProviders } from './providers';
 import './styles/global.css';
 
 export const App = () => {
-  return <AppProviders />;
+  return (
+    <>
+      <AppProviders />
+      <SpeedInsights />;
+    </>
+  );
 };
