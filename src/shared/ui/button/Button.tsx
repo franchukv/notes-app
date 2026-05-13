152697bc997e6837094ede4c81e1a2fb6eaa0d29
franchukv <franchuk.vldr@gmail.com>
2026-05-13 14:02:14 +0200

    fixed Button classNames variable

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -24,27 +24,26 @@ export const Button = <T extends ElementType = 'button'>({
   variant = 'primary',
   ...restProps
 }: ButtonProps<T>) => {
-  const classNames = () =>
-    cn(
-      'flex items-center justify-center gap-2 text-center transition-all duration-300 cursor-pointer disabled:pointer-events-none [&>svg]:h-5 [&>svg]:w-5',
-      !variant.includes('link')
-        ? 'min-w-32.5 min-h-11.5 py-3 px-4 text-preset-4 rounded-lg border focus-visible:bg-blue-500 disabled:bg-neutral-100 disabled:text-neutral-300! disabled:border-neutral-100! dark:disabled:bg-neutral-700 dark:disabled:border-neutral-700!'
-        : 'text-preset-5',
-      variant === 'primary' &&
-        'text-white bg-blue-500 border-blue-500 not-focus-visible:hover:bg-blue-700 not-focus-visible:hover:border-blue-700 not-focus-visible:active:border-blue-700 not-focus-visible:active:bg-blue-700',
-      variant === 'secondary' &&
-        'text-neutral-600 bg-neutral-100 border-neutral-100 hover:bg-white active:bg-white hover:text-neutral-950 active:text-neutral-950 hover:border-neutral-300 active:border-neutral-300 focus-visible:bg-white focus-visible:text-neutral-950 focus-visible:border-neutral-950 dark:bg-neutral-600 dark:border-neutral-600 dark:text-neutral-200',
-      variant === 'border' &&
-        'text-neutral-950 bg-transparent border-neutral-300 hover:text-neutral-600 active:text-neutral-600 hover:bg-neutral-100 active:bg-neutral-100 hover:border-neutral-100 active:border-neutral-100 focus-visible:bg-white focus-visible:text-neutral-950 focus-visible:border-neutral-950 dark:text-white dark:border-neutral-600',
-      variant === 'red' &&
-        'text-white bg-red-500 border-red-500 not-focus-visible:hover:bg-red-700 not-focus-visible:hover:border-red-700 not-focus-visible:active:border-red-700 not-focus-visible:active:bg-red-700 focus-visible:bg-red-500',
-      variant === 'primary-link' &&
-        'text-blue-500 hover:text-blue-700 active:text-blue-700',
-      variant === 'secondary-link' &&
-        'text-neutral-600 hover:text-neutral-950 active:text-neutral-950 dark:text-neutral-300 dark:hover:text-neutral-600 dark:active:text-neutral-600',
-      isLoading && 'justify-center!',
-      className,
-    );
+  const classNames = cn(
+    'flex items-center justify-center gap-2 text-center transition-all duration-300 cursor-pointer disabled:pointer-events-none [&>svg]:h-5 [&>svg]:w-5',
+    !variant.includes('link')
+      ? 'min-w-32.5 min-h-11.5 py-3 px-4 text-preset-4 rounded-lg border focus-visible:bg-blue-500 disabled:bg-neutral-100 disabled:text-neutral-300! disabled:border-neutral-100! dark:disabled:bg-neutral-700 dark:disabled:border-neutral-700!'
+      : 'text-preset-5',
+    variant === 'primary' &&
+      'text-white bg-blue-500 border-blue-500 not-focus-visible:hover:bg-blue-700 not-focus-visible:hover:border-blue-700 not-focus-visible:active:border-blue-700 not-focus-visible:active:bg-blue-700',
+    variant === 'secondary' &&
+      'text-neutral-600 bg-neutral-100 border-neutral-100 hover:bg-white active:bg-white hover:text-neutral-950 active:text-neutral-950 hover:border-neutral-300 active:border-neutral-300 focus-visible:bg-white focus-visible:text-neutral-950 focus-visible:border-neutral-950 dark:bg-neutral-600 dark:border-neutral-600 dark:text-neutral-200',
+    variant === 'border' &&
+      'text-neutral-950 bg-transparent border-neutral-300 hover:text-neutral-600 active:text-neutral-600 hover:bg-neutral-100 active:bg-neutral-100 hover:border-neutral-100 active:border-neutral-100 focus-visible:bg-white focus-visible:text-neutral-950 focus-visible:border-neutral-950 dark:text-white dark:border-neutral-600',
+    variant === 'red' &&
+      'text-white bg-red-500 border-red-500 not-focus-visible:hover:bg-red-700 not-focus-visible:hover:border-red-700 not-focus-visible:active:border-red-700 not-focus-visible:active:bg-red-700 focus-visible:bg-red-500',
+    variant === 'primary-link' &&
+      'text-blue-500 hover:text-blue-700 active:text-blue-700',
+    variant === 'secondary-link' &&
+      'text-neutral-600 hover:text-neutral-950 active:text-neutral-950 dark:text-neutral-300 dark:hover:text-neutral-600 dark:active:text-neutral-600',
+    isLoading && 'justify-center!',
+    className,
+  );
 
   return (
     <Tag
