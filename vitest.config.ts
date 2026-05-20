ca86af6ae8e7f0bf947e8a70b4f079b9eb5b8e31
franchukv <franchuk.vldr@gmail.com>
2026-05-20 13:48:08 +0200

    added vitest and react testing library, added setup for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: 'jsdom',
+    setupFiles: ['./src/shared/lib/tests/setup.ts'],
     env: {
       TZ: 'UTC',
     },
   },
+  plugins: [
+    {
+      name: 'svg-mock',
+      transform(_, id) {
+        if (id.endsWith('.svg?react')) {
+          return {
+            code: 'export default () => null',
+            map: null,
+          };
+        }
+      },
+    },
+  ],
 });
