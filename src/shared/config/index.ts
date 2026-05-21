654f4748cc4980afdff9e9dd3b3023057b4acb7f
franchukv <franchuk.vldr@gmail.com>
2026-05-21 12:49:26 +0200

    moved SITE_URL definition fom config into env file

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 export const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
-export const SITE_URL = 'https://notes-app-bcks.vercel.app';
+export const SITE_URL = import.meta.env.VITE_SITE_URL;
